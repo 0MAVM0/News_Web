fe8f55e2366cb397f3573441c94b3a883b065ec8
0MAVM0 <ediconpts9@gmail.com>
2025-04-17 18:03:48 +0500

    First Model - Category

--- config/news/models.py
+++ config/news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False, verbose_name="Category")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
