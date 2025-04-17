1ab872638384ee0f0a3a202078b9fdf61daacef7
0MAVM0 <ediconpts9@gmail.com>
2025-04-17 18:22:25 +0500

    Second Model (Main One) - News

--- config/news/models.py
+++ config/news/models.py
@@ -1,9 +1,36 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, null=False, blank=False, verbose_name="Category")
+    name = models.CharField(max_length=150, null=False, blank=False, unique=True, verbose_name="Category's Name")
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def __str__(self) -> str:
         return f"{self.name}"
+
+class News(models.Model):
+    class Status(models.TextChoices):
+        Draft = "DF", "Draft"
+        Published = "PB", "Published"
+
+    title = models.CharField(max_length=255, null=False, blank=False, verbose_name="Title Of The News")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="news/", null=True, blank=True)
+    body = models.TextField()
+    slug = models.SlugField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.Draft)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
+
+    class Meta:
+        ordering = ["-published_at"]
+        verbose_name = "News"
+        verbose_name_plural = "News"
